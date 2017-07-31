a7e6b5b1231b3c1008f2000867debfada67c3cd3
sumitdev <sumit-dev1611>
2017-07-31 14:52:25 +0530

    Passport.js used for athentication

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ var app = express();
 var bodyParser = require('body-parser');
 var routes = require('./routes/index.js');
 var db = require('./mongodb/db.js');
+var passport = require('passport');
+var Strategy = require('passport-http-bearer').Strategy;
+var jwt = require('jsonwebtoken');
 
-app.use(db())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+passport.use(new Strategy(function(token, cb) {
+    jwt.verify(token, "abc", function(err, access_token_data) {
+        if (err) {
+            return callback(err, null);
+        } else {
+            db.user.find({ _id: access_token_data.user_id }).exec(function(err, user) {
+                if (err) {
+                    return cb(err, null);
+                } else if (!user) {
+                    return cb(null, false);
+                } else {
+                    return cb(null, user);
+                }
+            });
+        }
+    });
+}));
+
 app.use('/', routes);
 app.use(errorHandler);
 
